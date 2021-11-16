cd1750f4df6f6074e7d727a8e8ba644190e33e45
Kelvin <kelvinkamau387@gmail.com>
2021-11-16 23:30:13 +0300

    Add jsx for cta section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,7 +1,17 @@
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>request early access to get started</p>
+        <h3>register today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">get started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
